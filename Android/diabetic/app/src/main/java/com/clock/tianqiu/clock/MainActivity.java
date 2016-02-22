1ca8c85f2b448b69539bea79df06fe06f31d8682
Tian Qiu <qiu58@purdue.edu>
2016-02-21 19:55:23 -0500

    DURI Research android app start
    Host tab finished for four states

---
+++ Android/diabetic/app/src/main/java/com/clock/tianqiu/clock/MainActivity.java
@@ -0,0 +1,80 @@
+package com.clock.tianqiu.clock;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("Tab1").setIndicator("ImageInput").setContent(R.id.ImageInput));
+        tabHost.addTab(tabHost.newTabSpec("Tab2").setIndicator("ha").setContent(R.id.table2));
+        tabHost.addTab(tabHost.newTabSpec("Tab3").setIndicator("3").setContent(R.id.table3));
+        tabHost.addTab(tabHost.newTabSpec("Tab4").setIndicator("ImageResult").setContent(R.id.ImageResult));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // new
+
+    private TabHost     tabHost;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
