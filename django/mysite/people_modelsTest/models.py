3b21c4e172b74caff6f16bd8956eccd0b3292478
Tian Qiu <qiu58@purdue.edu>
2016-04-02 13:58:23 -0400

    form test，database initialized

--- django/mysite/people_modelsTest/models.py
+++ django/mysite/people_modelsTest/models.py
@@ -7,3 +7,6 @@ from django.db import models
 class Person(models.Model):            # person has name and age
     name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):     # return 从object 改成 person's name
+        return self.name
