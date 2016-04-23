f76c698b0a6f55b1e2eeca3111b93854c2f0ea7f
Tian Qiu <qiu58@purdue.edu>
2016-04-23 15:47:34 -0400

    blog admin added

--- django/mysite/blog/admin.py
+++ django/mysite/blog/admin.py
@@ -16,6 +16,19 @@ class MyModelAdmin(admin.ModelAdmin):
         else:
             return qs.filter(author=request.user)
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    search_fields = ('name',)
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(PersonAdmin, self).get_search_results(request, queryset, search_term)
+        try:
+            search_term_as_int = int(search_term)
+            queryset |= self.model.objects.filter(age=search_term_as_int)
+        except:
+            pass
+        return queryset, use_distinct
+
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date', 'update_time',)
